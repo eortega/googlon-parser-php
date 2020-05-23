56016aa3f4c7a12492a0f31db6f57cafb83edc1f
Edgar Ortega <edgar.ortega@tala.co>
2020-05-23 13:02:37 -0500

    PHPUnit added to the repo (#1)
    * Loas classes with PSR-4
    
    * Test cases using phpunit
    
    * Update test instructions in readme

--- tests/GooglonParserTest.php
+++ tests/GooglonParserTest.php
@@ -1,111 +1,64 @@
 <?php
-require_once 'src/GooglonParser.php';
+declare(strict_types=1);
+use GooglonParser\GooglonParser;
+use PHPUnit\Framework\TestCase;
 
-class GooglonParserTest
+class GooglonParserTest extends TestCase
 {
 
-    protected $testCases = null;
-
-    public function start()
+    protected static function loadCases()
     {
-        $this->loadCases();
+        return include 'TestCases.php';
+    }
 
-        foreach($this->testCases as $caseNum=>$text) {
-            $this->executeTestCase($caseNum, $text);
+    public function testPrepositions()
+    {
+        $testCases = self::loadCases();
+        foreach($testCases as $case) {
+            $tokens = GooglonParser::tokenize($case['text']);
+            $actualPrepositions = array_filter($tokens, 'GooglonParser\GooglonParser::isPreposition');
+            self::assertCount($case['e']['propositions'], $actualPrepositions);
         }
-
     }
 
-    protected function executeTestCase(string $caseNumber, array $testCase)
+    public function testVerbs()
     {
-        echo "Case {$caseNumber} \n";
-        $tokens = GooglonParser::tokenize($testCase['text']);
-
-        // PREPOSOTIONS
-        $prepositions = array_filter($tokens,'GooglonParser::isPreposition');
-        $this->assertIntWithMessage('Prepositions',$testCase['e']['propositions'],count($prepositions));
-
-        //VERBS
-        $verbs = array_filter($tokens,'GooglonParser::isVerb');
-        $this->assertIntWithMessage('Verbs',$testCase['e']['verbs'],count($verbs));
-
-        // SUBJUNCTIVE VERBS
-        $subjVerbs = array_filter($tokens,'GooglonParser::isSubjunctiveVerb');
-        $this->assertIntWithMessage('Subjunctive Verbs',$testCase['e']['subjVerbs'],count($subjVerbs));
-
-        //PRETTY NUMBERS
-        $numbersList = array_map('GooglonParser::wordToNumber', $tokens);
-        $prettyNumbers = array_filter($numbersList,'GooglonParser::isPrettyNumber');
-        $this->assertIntWithMessage('Unique Pretty Numbers',$testCase['e']['distinctPrettyNumbers'],count(array_unique($prettyNumbers)));
-
-
-        //$vocabularyList = GooglonParser::lexicographicalSort(array_unique($tokens));
-        //echo sprintf("There are %s Propoistions \n", count($prepositions));
-        $lexicallyOrdered = GooglonParser::radix(array_unique($tokens));
-        $this->assertSortedList('Vocabulary List',$testCase['e']['sorted'], $lexicallyOrdered);
-        //$this->printLists($testCase['e']['sorted'], $vocabularyList);
-        echo "\n\n";
-    }
-
-    protected function assertIntWithMessage(string $message, int $expected, int $real)
-    {
-        $text = sprintf("%s: Expected %s and Received %s\n", $message, $expected, $real);
-        //Red by default
-        $cliColor = "\e[0;31;m%s\e[0m";
-        if($expected == $real) {
-            $cliColor = "\e[0;32;m%s\e[0m";
+        $testCases = self::loadCases();
+        foreach($testCases as $case) {
+            $tokens = GooglonParser::tokenize($case['text']);
+            $actualVerbs = array_filter($tokens, 'GooglonParser\GooglonParser::isVerb');
+            self::assertCount($case['e']['verbs'], $actualVerbs);
         }
-
-        echo sprintf($cliColor, $text);
     }
 
-    protected function printLists(array $expected, array $real)
+    public function testSubjunctiveVerbs()
     {
-        $m = count($expected) >= count($real) ? count($expected) : count($real);
-        echo "E\t | R\n";
-        for($i=0; $i<$m; $i++) {
-            $ev = $expected[$i] ?? '_';
-            $rv = $real[$i] ?? '_';
-            echo sprintf("%s \t\t\t| %s\n",$ev, $rv);
+        $testCases = self::loadCases();
+        foreach($testCases as $case) {
+            $tokens = GooglonParser::tokenize($case['text']);
+            $actualSubjVerbs = array_filter($tokens, 'GooglonParser\GooglonParser::isSubjunctiveVerb');
+            self::assertCount($case['e']['subjVerbs'], $actualSubjVerbs);
         }
-
     }
 
-    protected function assertSortedList(string $message, array $expected, array $real)
+    public function testPrettyNumbers()
     {
-        $listsSizeMessage = sprintf("%s: Expected %s and Received %s\n", $message, count($expected), count($real));
-        $listSortMessage = sprintf("%s: Sorted\n", $message);
-        //Red by default
-        $cliColorListSize = "\e[0;31;m%s\e[0m";
-        $cliColorListSorted = "\e[0;32;m%s\e[0m";
-
-        //Verify List size
-        if(count($expected) == count($real)) {
-            $cliColorListSize = "\e[0;32;m%s\e[0m";
-
-            for($i=0; $i< count($expected); $i++) {
-                if($expected[$i] != $real[$i]) {
-                    $cliColorListSorted = "\e[0;31;m%s\e[0m";
-                    echo "$expected[$i]:: $real[$i]\n";
-                    //$sorted=false;
-                    break;
-                }
-            }
-
+        $testCases = self::loadCases();
+        foreach($testCases as $case) {
+            $tokens = GooglonParser::tokenize($case['text']);
+            $numbersList = array_map('GooglonParser\GooglonParser::wordToNumber', $tokens);
+            $prettyNumbers = array_filter($numbersList, 'GooglonParser\GooglonParser::isPrettyNumber');
+            self::assertCount($case['e']['distinctPrettyNumbers'], $prettyNumbers);
         }
-
-        echo sprintf($cliColorListSize, $listsSizeMessage);
-        echo sprintf($cliColorListSorted, $listSortMessage);
-        //echo sprintf($cliColorListSIze, $itemNumber);
-
     }
 
-    protected function loadCases()
+    public function testLexicographicalSort()
     {
-        $this->testCases = include 'TestCases.php';
+        $testCases = self::loadCases();
+        foreach($testCases as $case) {
+            $tokens = GooglonParser::tokenize($case['text']);
+            $lexicallyOrdered = GooglonParser::lexicographicalSort(array_unique($tokens));
+            self::assertEquals($case['e']['sorted'], $lexicallyOrdered);
+        }
     }
 }
-
-
-$t = new GooglonParserTest();
-$t->start();
