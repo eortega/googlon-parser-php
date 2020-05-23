7911371ef2e1e62ef39c16c1760ee832f9755123
Edgar Ortega <edgar.ortega@tala.co>
2020-05-23 13:34:05 -0500

    Release v1.0.1 (#3)
    * Add phpunit
    
    * PHPUnit added to the repo (#1)
    
    * Loas classes with PSR-4
    
    * Test cases using phpunit
    
    * Update test instructions in readme
    
    * Fix psr2 violations (#2)

---
+++ tests/GooglonParserTest.php
@@ -0,0 +1,64 @@
+<?php
+declare(strict_types=1);
+use GooglonParser\GooglonParser;
+use PHPUnit\Framework\TestCase;
+
+class GooglonParserTest extends TestCase
+{
+
+    protected static function loadCases()
+    {
+        return include 'TestCases.php';
+    }
+
+    public function testPrepositions()
+    {
+        $testCases = self::loadCases();
+        foreach ($testCases as $case) {
+            $tokens = GooglonParser::tokenize($case['text']);
+            $actualPrepositions = array_filter($tokens, 'GooglonParser\GooglonParser::isPreposition');
+            self::assertCount($case['e']['propositions'], $actualPrepositions);
+        }
+    }
+
+    public function testVerbs()
+    {
+        $testCases = self::loadCases();
+        foreach ($testCases as $case) {
+            $tokens = GooglonParser::tokenize($case['text']);
+            $actualVerbs = array_filter($tokens, 'GooglonParser\GooglonParser::isVerb');
+            self::assertCount($case['e']['verbs'], $actualVerbs);
+        }
+    }
+
+    public function testSubjunctiveVerbs()
+    {
+        $testCases = self::loadCases();
+        foreach ($testCases as $case) {
+            $tokens = GooglonParser::tokenize($case['text']);
+            $actualSubjVerbs = array_filter($tokens, 'GooglonParser\GooglonParser::isSubjunctiveVerb');
+            self::assertCount($case['e']['subjVerbs'], $actualSubjVerbs);
+        }
+    }
+
+    public function testPrettyNumbers()
+    {
+        $testCases = self::loadCases();
+        foreach ($testCases as $case) {
+            $tokens = GooglonParser::tokenize($case['text']);
+            $numbersList = array_map('GooglonParser\GooglonParser::wordToNumber', $tokens);
+            $prettyNumbers = array_filter($numbersList, 'GooglonParser\GooglonParser::isPrettyNumber');
+            self::assertCount($case['e']['distinctPrettyNumbers'], $prettyNumbers);
+        }
+    }
+
+    public function testLexicographicalSort()
+    {
+        $testCases = self::loadCases();
+        foreach ($testCases as $case) {
+            $tokens = GooglonParser::tokenize($case['text']);
+            $lexicallyOrdered = GooglonParser::lexicographicalSort(array_unique($tokens));
+            self::assertEquals($case['e']['sorted'], $lexicallyOrdered);
+        }
+    }
+}
